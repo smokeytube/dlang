567e07b477aa7d57a192b70cf074e703ad670711
Zachary Davis <67290867+smokeytube@users.noreply.github.com>
2022-03-06 11:21:37 +0200

    Update CubeEntity.java

--- src/main/java/smokeytube/dlang/entity/CubeEntity.java
+++ src/main/java/smokeytube/dlang/entity/CubeEntity.java
@@ -1,11 +1,24 @@
 package smokeytube.dlang.entity;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.EscapeDangerGoal;
+import net.minecraft.entity.ai.goal.LookAroundGoal;
+import net.minecraft.entity.ai.goal.LookAtEntityGoal;
+import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
 import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 
 public class CubeEntity extends PathAwareEntity {
     public CubeEntity(EntityType<? extends PathAwareEntity> entityType, World world) {
         super(entityType, world);
+        this.experiencePoints = 10;
+        this.knockbackVelocity = 5f;
     }
+	protected void initGoals() {
+		this.goalSelector.add(0, new WanderAroundFarGoal(this, 1.0D));
+        this.goalSelector.add(1, new LookAroundGoal(this));
+        this.goalSelector.add(2, new LookAtEntityGoal(this, PlayerEntity.class, 6.0F));
+        this.goalSelector.add(3, new EscapeDangerGoal(this, 1.25D));
+	}
 }
